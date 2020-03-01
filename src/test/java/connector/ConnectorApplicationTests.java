787ed49c72aa917f79637f3a7661429e6e1edf92
kasper084 <kasper084@gmail.com>
2020-03-01 17:55:40 +0200

    start

---
+++ src/test/java/connector/ConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
