c25ab89684f784a01d918c21b380a79bd944e507
kasper084 <kasper084@gmail.com>
2020-03-03 10:20:14 +0200

    new application.properties

--- src/main/java/connector/controller/Controller.java
+++ src/main/java/connector/controller/Controller.java
@@ -1,4 +1,18 @@
 package connector.controller;
 
+import connector.service.AccountServiceImpl;
+import connector.service.ProfilesServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @Autowired
+    AccountServiceImpl accountService;
+
+    @Autowired
+    ProfilesServiceImpl profilesService;
+
+    
 }
